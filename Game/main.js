88d1df1efa7a17e937d1699f5629b31400244b1d
Squirrel-314 <schoolarlybay@gmail.com>
2021-01-02 15:33:15 -0500

    Many Things
    It's been a while since the last commit.

--- Game/main.js
+++ Game/main.js
@@ -2,6 +2,29 @@
 // Copyright Janurary 1st 2021
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-let discoveredVegtables = [
+let discoveredVegtables = {
    key: "value",
-]
+}
+
+function plantPeas() {
+   setInterval(growing, 2000);
+   //setInterval(harvestReadyPeas, 5000);
+}
+
+function growing() {
+   document.getElementById("plot1").style.background = "url(D:/Documents/GitHub/vegetable-dash/Images/growing.png)";
+   document.getElementById("plot1").style.backgroundSize = "cover";
+}
+
+function harvestReadyPeas() {
+   document.getElementById("plot1").style.background = "url(D:/Documents/GitHub/vegetable-dash/Images/grow-pea.png)";
+   document.getElementById("plot1").style.backgroundSize = "cover";
+   document.getElementById("plant1").style.display = "none";
+   document.getElementById("harvest1").style.display = "block";
+}
+
+function harvest() {
+
+}
+
+window.onload = plantPeas();
