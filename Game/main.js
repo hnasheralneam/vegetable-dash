2a6bc3e64b1e86b9a25d66c76a5180fd9fadae0d
Squirrel-314 <schoolarlybay@gmail.com>
2021-01-02 17:50:48 -0500

    Oh dearie me, that's a lot of files.

--- Game/main.js
+++ Game/main.js
@@ -2,29 +2,68 @@
 // Copyright Janurary 1st 2021
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-let discoveredVegtables = {
-   key: "value",
+let plotStatus = {
+   peas: "empty",
+   corn: "empty",
 }
 
-function plantPeas() {
-   setInterval(growing, 2000);
-   //setInterval(harvestReadyPeas, 5000);
-}
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// Communal Functions
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 function growing() {
-   document.getElementById("plot1").style.background = "url(D:/Documents/GitHub/vegetable-dash/Images/growing.png)";
-   document.getElementById("plot1").style.backgroundSize = "cover";
-}
-
-function harvestReadyPeas() {
-   document.getElementById("plot1").style.background = "url(D:/Documents/GitHub/vegetable-dash/Images/grow-pea.png)";
-   document.getElementById("plot1").style.backgroundSize = "cover";
-   document.getElementById("plant1").style.display = "none";
-   document.getElementById("harvest1").style.display = "block";
+   plotStatus.peas = "growing";
 }
 
 function harvest() {
-
+   plotStatus.peas = "empty";
+   document.getElementById("grow1").style.display = "block";
+   document.getElementById("harvest1").style.display = "none";
 }
 
-window.onload = plantPeas();
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// Peas
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+function plantPeas() {
+   setTimeout(growing, 2000);
+   setTimeout(harvestReadyPeas, 5000);
+   document.getElementById("grow1").style.display = "none";
+}
+
+function harvestReadyPeas() {
+   plotStatus.peas = "ready";
+}
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// Status
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+function peaStatus() {
+   if (plotStatus.peas === "ready") {
+      document.getElementById("plot1").style.background = "url(../Images/Plots/grown-pea.png)";
+      document.getElementById("plot1").style.backgroundSize = "cover";
+      document.getElementById("harvest1").style.display = "block";
+   }
+   else if (plotStatus.peas === "growing") {
+      document.getElementById("plot1").style.background = "url(../Images/Plots/growing.png)";
+      document.getElementById("plot1").style.backgroundSize = "cover";
+   }
+   else {
+      document.getElementById("plot1").style.background = "url(../Images/Plots/plot.png)";
+      document.getElementById("plot1").style.backgroundSize = "cover";
+   }
+}
+
+// Check Plant Status
+var plantStatus = window.setInterval(function() {
+   peaStatus();
+}, 200)
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// just Because
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+function openLock() {
+   // Opening lock animation
+}
