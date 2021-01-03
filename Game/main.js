9d62c55bc107a391f50ca2c103097c74ffb0a14f
Squirrel-314 <schoolarlybay@gmail.com>
2021-01-02 19:14:12 -0500

    Trying to get timer to work

--- Game/main.js
+++ Game/main.js
@@ -7,6 +7,11 @@ let plotStatus = {
    corn: "empty",
 }
 
+let produce = {
+   peas: 0,
+   corn: 0,
+}
+
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // Communal Functions
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -19,7 +24,82 @@ function harvest() {
    plotStatus.peas = "empty";
    document.getElementById("grow1").style.display = "block";
    document.getElementById("harvest1").style.display = "none";
+   produce.peas++;
 }
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// Crop Time Remaining
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+/*
+let hour, min, sec;
+
+function peaTime() {
+   hour = 0;
+   min = 0;
+   sec = 5;
+   setInterval(function() {
+      document.getElementById("peaTimeRemaining").innerHTML = `${hour} hours ${min} minutes ${sec} seconds`;
+      sec--;
+      if (sec === 00 && hour > 0) {
+         min --;
+         sec = 60;
+      }
+      if (min === 00 && hour > 0) {
+         hour --;
+         min = 60;
+      }
+      if (sec <= 0 && min === 0 && hour === 0 && plotStatus.peas === "empty") {
+         document.getElementById("peaTimeRemaining").innerHTML = "...";
+      }
+      else if (sec <= 0 && min === 0 && hour === 0) {
+         document.getElementById("peaTimeRemaining").innerHTML = "Done!";
+      }
+   }, 1000);
+}
+*/
+
+function getTimeRemaining(endtime) {
+  const total = Date.parse(endtime) - Date.parse(new Date());
+  const seconds = Math.floor((total / 1000) % 60);
+  const minutes = Math.floor((total / 1000 / 60) % 60);
+  const hours = Math.floor((total / (1000 * 60 * 60)) % 24);
+  const days = Math.floor(total / (1000 * 60 * 60 * 24));
+
+  return {
+    total,
+    days,
+    hours,
+    minutes,
+    seconds
+  };
+}
+
+function initializeClock(id, endtime) {
+  const clock = document.getElementById(id);
+  const daysSpan = clock.querySelector('.days');
+  const hoursSpan = clock.querySelector('.hours');
+  const minutesSpan = clock.querySelector('.minutes');
+  const secondsSpan = clock.querySelector('.seconds');
+
+  function updateClock() {
+    const t = getTimeRemaining(endtime);
+
+    daysSpan.innerHTML = t.days;
+    hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
+    minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
+    secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
+
+    if (t.total <= 0) {
+      clearInterval(timeinterval);
+    }
+  }
+
+  updateClock();
+  const timeinterval = setInterval(updateClock, 1000);
+}
+
+const deadline = new Date(Date.parse(new Date()) + 15 * 24 * 60 * 60 * 1000);
+initializeClock('clockdiv', deadline);
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // Peas
@@ -29,6 +109,10 @@ function plantPeas() {
    setTimeout(growing, 2000);
    setTimeout(harvestReadyPeas, 5000);
    document.getElementById("grow1").style.display = "none";
+   // peaTime();
+   // hour = 0;
+   // min = 0;
+   // sec = 5;
 }
 
 function harvestReadyPeas() {
@@ -58,10 +142,19 @@ function peaStatus() {
 // Check Plant Status
 var plantStatus = window.setInterval(function() {
    peaStatus();
+   produceDisplay();
 }, 200)
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-// just Because
+// Fresh Produce | Store
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+function produceDisplay() {
+   document.getElementById("peaBushels").innerHTML = `${produce.peas} Bushels of Peas`
+}
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// Just Because
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 function openLock() {
