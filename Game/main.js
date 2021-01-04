757723fe9940210ab3133bac09920ea10c4516b6
Squirrel-314 <schoolarlybay@gmail.com>
2021-01-04 08:57:31 -0500

    Many changes!
    Restart button, fixed save, started on plot 3.

--- Game/main.js
+++ Game/main.js
@@ -8,6 +8,7 @@ Peas               | All about the first plot
 Store              | Update the store
 Purchase Plots     | Functiona that unlock plots
 Setup              | Prepare game for returning player
+Settings           | So far, just restart
 Save               | Save the game data
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
 // Initate JavaScript strict mode
@@ -18,19 +19,19 @@ Game Data
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
 
 // Set plot status of each plot (Empty, growing, done)
-let plotStatus = {
+let initalPlotStatus = {
    peas: "empty",
    corn: "empty",
 }
 
 // Set the amount of player produce
-let produce = {
+let initalProduce = {
    peas: 0,
    corn: 0,
 }
 
 // Set the prices of each plot
-let plots = {
+let initalPlots = {
    // Plot Prices
    plot1Price: 0,
    plot2Price: 25,
@@ -41,7 +42,14 @@ let plots = {
    plot7Price: "undeterimed",
    plot8Price: "undeterimed",
    plot9Price: "undeterimed",
+   // Unlocked/locked
+   peaplot: "unlocked",
+   cornplot: "locked",
 }
+
+let plotStatus = initalPlotStatus;
+let produce = initalProduce;
+let plots = initalPlots;
 
 /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // Peas
@@ -152,6 +160,52 @@ function cornStatus() {
 }
 
 /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// Strawberries
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+
+function plantStrawberries() {
+   setTimeout(sproutingStrawberries, 20000);
+   setTimeout(floweringStrawberries, 60000);
+   setTimeout(fruitingStrawberries, 120000);
+   document.getElementById("grow3").style.opacity = "0";
+}
+
+function sproutingStrawberries() {plotStatus.strawberries = "sprouting";}
+function floweringStrawberries() {plotStatus.strawberries = "growing";}
+function fruitingStrawberries() {plotStatus.strawberries = "growing";}
+
+function harvestStrawberries() {
+   plotStatus.corn = "empty";
+   document.getElementById("grow3").style.opacity = "1";
+   document.getElementById("harvest3").style.opacity = "0";
+   document.getElementById("harvest3").style.zIndex = "-1";
+   document.getElementById("grow3").style.zIndex = "1";
+   produce.strawberries++;
+}
+
+function strawberriesStatus() {
+   if (plotStatus.strawberries === "ready") {
+      document.getElementById("plot3").style.background = "url(../Images/Plots/grown-corn.png)";
+      document.getElementById("plot3").style.backgroundSize = "cover";
+      document.getElementById("harvest3").style.opacity = "1";
+      document.getElementById("harvest3").style.zIndex = "1";
+      document.getElementById("grow3").style.zIndex = "-1";
+   }
+   else if (plotStatus.strawberries === "sprouting") {
+      document.getElementById("plot3").style.background = "url(../Images/Plots/growing.png)";
+      document.getElementById("plot3").style.backgroundSize = "cover";
+   }
+   else if (plotStatus.strawberries === "sprouting") {
+      document.getElementById("plot3").style.background = "url(../Images/Plots/growing.png)";
+      document.getElementById("plot3").style.backgroundSize = "cover";
+   }
+   else {
+      document.getElementById("plot3").style.background = "url(../Images/Plots/plot.png)";
+      document.getElementById("plot3").style.backgroundSize = "cover";
+   }
+}
+
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // Purchase Plots
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
 
@@ -171,6 +225,8 @@ function openLock() {
    document.getElementById("lock2").classList.add("removing-lock");
    // Remove the lock in 2.5 seconds, the amount of time it takes for the animaiton
    setTimeout(removeLock, 2500);
+   // Unlock third plot purchase
+   document.getElementById("lock3Text").innerHTML = 'This plot is locked <br> Pay 50 bushels of peas and 25 bushels of corn to unlock <br> <button class="purchase-plot" onclick="purchasePlot3()">Purchase Plot</button>';
 }
 
 function removeLock() {
@@ -180,6 +236,8 @@ function removeLock() {
    lock2.remove();
    // And reveal the div beneath
    document.getElementById("openPlot2").style.display = "block";
+   // Add item to plots
+   plots.cornplot = "unlocked";
 }
 
 /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -207,10 +265,47 @@ var plantStatus = window.setInterval(function() {
 function setup() {
    // Run product display
    produceDisplay();
+
+   if (plots.cornplot === "unlocked") {
+      setTimeout(openLock, 500);
+   }
 }
 
 // Run function setup when page loads
-window.onload = setup();
+window.addEventListener('load', (event) => {
+   setup();
+});
+
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// Settings
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+
+function restart() {
+   plotStatus = initalPlotStatus;
+   produce = initalProduce;
+   plots = initalPlots;
+}
+
+function restart() {
+   // Confirm Restart
+   var areYouSure = confirm("Are you SURE you want to restart? This will wipe all your progress!");
+   // If restart is confirmed
+   if (areYouSure == true) {
+      // Ask again
+      var areYouReallySure = confirm("Are you REALLY SURE you want to restart? There is no going back!");
+      // If restart is still confirmed
+      if (areYouReallySure == true) {
+         // Set game data to inital values
+         plotStatus = initalPlotStatus;
+         produce = initalProduce;
+         plots = initalPlots;
+         // Set save as blank
+         localStorage.setItem("plotStatus", JSON.stringify(plotStatus));
+         localStorage.setItem("produce", JSON.stringify(produce));
+         localStorage.setItem("plots", JSON.stringify(plots));
+      }
+   }
+}
 
 /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // Save
@@ -225,9 +320,9 @@ var saveLoop = window.setInterval(function() {
 
 // Find the items from loacl storage and assign to key
 var savegame = {
-   plotStatus: JSON.parse(localStorage.getItem("produce")),
+   plotStatus: JSON.parse(localStorage.getItem("plotStatus")),
    produce: JSON.parse(localStorage.getItem("produce")),
-   plots: JSON.parse(localStorage.getItem("produce")),
+   plots: JSON.parse(localStorage.getItem("plots")),
 }
 
 // Set varibles as the saved items
@@ -241,4 +336,8 @@ if (savegame !== null) {
    savegame.plotStatus = plotStatus;
    savegame.produce = produce;
    savegame.plots = plots;
+   // Then set varibles to saved items
+   plotStatus = savegame.plotStatus;
+   produce = savegame.produce;
+   plots = savegame.plots;
 }
