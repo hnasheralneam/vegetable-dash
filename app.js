0b2d521b2a73700241d8f6baf867ccdc5f91ad74
Squirrel <thehillsidetownship@gmail.com>
2021-12-31 21:59:10 +0000

    Hide MongoDB Password

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ mongoose.set("useFindAndModify", false);
 
 // Mongoose things
 mongoose.Promise = global.Promise;
-mongoose.connect("mongodb+srv://Squirrel:HgtSzuyB8ookIVHq@test-user-data.daqv1.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", { useUnifiedTopology: true, useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI, { useUnifiedTopology: true, useNewUrlParser: true });
 
 // System things
 connection.on('error', console.error.bind(console, 'Connection error: '));
